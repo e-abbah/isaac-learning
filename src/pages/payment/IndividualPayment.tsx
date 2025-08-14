4b60538e9fd134b3c9b57d88a927d24f3a45d981
Majiyebo Esuga <majiyeboesuga8@gmail.com>
2025-08-14 16:51:00 +0100

    Add validation to cardnumber, name, cvv and expiry date fields

--- src/pages/payment/IndividualPayment.tsx
+++ src/pages/payment/IndividualPayment.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react"
+import { useState, useEffect } from "react"
 import { IndividualForm, FamilyForm } from "../../components/payment/PaymentForms"
 import { usePayment } from "../../context/PaymentContext";
 
@@ -6,18 +6,23 @@ import BackButton from "../../components/back-button/BackButton";
 
 
 function IndividualPayment() {
-  const [isFamily, setIsFamily] = useState(false)
   const [cardName, setCardName] = useState('')
   const [cardNumber, setCardNumnber] = useState('')
   const [expiryDate, setExpiryDate] = useState('')
   const [cvv, setCvv] = useState('')
   const { selectedCourse, selectedPayment } = usePayment()
   const storedPlan = localStorage.getItem("paymentPlan")
-  const nameValid = /^([a-z]+)(\s)([a-z]+)(?:( )([a-z]+))?$/i
-  const numberValid = /^\d+$/
-  const expiryCheck = /^$/
-  console.log(localStorage.getItem('paymentPlan'))
+  // const nameValid = /^([a-z]+)(\s)([a-z]+)(?:( )([a-z]+))?$/i
+  // const numberValid = /d+/
+  // const expiryValid = /d+/
+  // const cvvValid = /d+/
+  const [isFormFilled, setIsFormFilled] = useState(new Array(4).fill(''))
+  const [canSubmit, setCanSubmit] = useState(false)
 
+  useEffect(() => {
+    const allFilled = isFormFilled.every(val => val.trim() !== "");
+    setCanSubmit(allFilled);
+  }, [isFormFilled]);
 
   return (
     <div className="text-center py-20 px-20">
@@ -43,9 +48,13 @@ function IndividualPayment() {
               <p className="text-sub-gray text-xs text-left mb-2">Card name</p>
               <input type="text" name="" id="" className={`border-1 px-4 py-[10px] mb-6 w-full rounded-[10px] focus:outline-primary-green ${cardName ? 'outline-primary-green border-primary-green ' : 'border-sub-gray'}`}
                 value={cardName}
-                onChange={(e) => setCardName(e.target.value)}
+                onChange={(e) => {
+                  setCardName(e.target.value);
+                  setIsFormFilled(prev => prev.map((form, i) => i == 0 ? e.target.value : form))
+                  console.log(cvv, ' ', expiryDate, ' ', cardName, ' ', cardNumber)
+
+                }}
               />
-              {/* {nameValid.test(cardName) ? '' : <p className="text-red-600 text-sm font-bold">fix this</p>} */}
             </div>
             <div className=" w-full">
               <p className="text-sub-gray text-xs text-left mb-2">Card number</p>
@@ -54,8 +63,11 @@ function IndividualPayment() {
                 onChange={(e) => {
                   const value = e.target.value.replace(/\D/g, "").slice(0, 16);
                   let display = value
-                  value.length > 12 ? (display = value.slice(0, 4) + ' - ' + value.slice(4, 8) + ' - ' + value.slice(8, 12) + ' - ' + value.slice(12, 16)) : console.log('hi')
+                  value.length > 12 ? (display = value.slice(0, 4) + ' - ' + value.slice(4, 8) + ' - ' + value.slice(8, 12) + ' - ' + value.slice(12, 16)) : ''
                   setCardNumnber(display);
+                  setIsFormFilled(prev => prev.map((form, i) => i == 1 ? e.target.value : form))
+                  console.log(cvv, ' ', expiryDate, ' ', cardName, ' ', cardNumber)
+
                 }}
               />
             </div>
@@ -67,8 +79,11 @@ function IndividualPayment() {
                   onChange={(e) => {
                     const value = e.target.value.replace(/\D/g, "").slice(0, 4);
                     let display = value
-                    value.length > 2 ? (display = value.slice(0, 2) + ' / ' + value.slice(2, 4), console.log(value.length)) : console.log(value)
+                    value.length > 2 ? (display = value.slice(0, 2) + ' / ' + value.slice(2, 4)) : ''
                     setExpiryDate(display);
+                    setIsFormFilled(prev => prev.map((form, i) => i == 2 ? e.target.value : form))
+                    console.log(cvv, ' ', expiryDate, ' ', cardName, ' ', cardNumber)
+
                   }}
                 />
               </div>
@@ -79,6 +94,8 @@ function IndividualPayment() {
                   onChange={(e) => {
                     const value = e.target.value.replace(/\D/g, "").slice(0, 3);
                     setCvv(value);
+                    setIsFormFilled(prev => prev.map((form, i) => i == 3 ? e.target.value : form))
+                    console.log(cvv, ' ', expiryDate, ' ', cardName, ' ', cardNumber)
                   }}
                 />
               </div>
@@ -86,8 +103,8 @@ function IndividualPayment() {
           </div>
         </div>
         <div className="w-full  mt-2">
-          {storedPlan == 'Individual' ? <IndividualForm isFamily={isFamily} setIsFamily={setIsFamily} /> :
-            <FamilyForm isFamily={isFamily} setIsFamily={setIsFamily} />}
+          {(storedPlan == 'Individual') ? <IndividualForm canSubmit={canSubmit} /> :
+            <FamilyForm canSubmit={canSubmit} />}
         </div>
       </div>
     </div>
